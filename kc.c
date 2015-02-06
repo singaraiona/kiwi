72aaf04e31168143675f0713f6cd1dcbe87abdda
Anton Kundenko <volonter84@gmail.com>
2015-02-06 14:44:03 +0200

    initial commit

---
+++ kc.c
@@ -0,0 +1,49 @@
+/**
+*     File Name           :     j.c
+*     Created By          :     
+*     Creation Date       :     05-02-2015 12:29
+*     Description         :      
+***/
+
+#include <stdio.h>
+#include <string.h>
+#include <sys/mman.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+typedef unsigned I;typedef char C,*S;
+#define R return
+#define O printf
+#define V void
+
+#define OC(x) O("%c\n",x)
+#define OS(x) O("%s\n",x)
+#define OI(x) O("%d\n",x)
+#define OU(x) O("%u\n",x)
+#define OX(x) O("0x%X\n",x)
+
+#define RL(x) {I _c=0;while(_c=fgetc(stdin)){x(_c);}}
+#define SZ(x) sizeof(x)
+#define LN(x) strlen(x)
+
+I assign=0;
+I plus=1;
+I minus=2;
+
+typedef struct a0{I t; C v;struct m0 *r;} A;
+#define SA SZ(A)
+A *fnode = NULL;
+A *lnode = NULL;
+
+A *astinit() {R (A *)mmap(NULL, SA*10, PROT_READ|PROT_WRITE, MAP_PRIVATE|MAP_ANON, -1, 0);}
+I kinit() {fnode=astinit();if(!fnode)R 1;fnode->v=NULL;fnode->t=assign;fnode->r=NULL;lnode=fnode; R 0;}
+I newnode(a,b) {lnode->v=a;lnode->r=++lnode;lnode->t=b;lnode->r=NULL; R 0;}
+V kproc() {A *n=fnode;while(NULL!=n->r){OC(n->v);n++;} }
+V tok(c) {if(isdigit(c)){newnode(c,plus);}
+          else if (c==0x0A) {kproc();} }
+I main() {if(kinit())R 1;RL(tok);munmap(fnode,SA*10);R 0;}
+
+
